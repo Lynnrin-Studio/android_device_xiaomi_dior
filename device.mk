9c66b5ec292f1d8d2fd665ba135b50073f63d0be
TARKZiM <tom8476oo@gmail.com>
2020-11-17 13:28:35 +0800

    dior: Build Configstore HAL 1.1

--- device.mk
+++ device.mk
@@ -106,8 +106,8 @@ PRODUCT_COPY_FILES += \
 
 # Configstore
 PRODUCT_PACKAGES += \
-    android.hardware.configstore@1.0-impl \
-    android.hardware.configstore@1.0-service
+    android.hardware.configstore@1.1-impl \
+    android.hardware.configstore@1.1-service
 
 # Connectivity
 PRODUCT_PACKAGES += \

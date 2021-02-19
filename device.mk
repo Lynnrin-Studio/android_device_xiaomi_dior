71caa196c2948979b8f27f5ac303e36996bc339e
TARKZiM <tom8476oo@gmail.com>
2021-02-20 02:32:46 +0800

    dior: Remove unused inherit target

--- device.mk
+++ device.mk
@@ -26,7 +26,6 @@ PRODUCT_AAPT_PREBUILT_DPI := xhdpi 280dpi hdpi tvdpi mdpi ldpi
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/go_defaults.mk)
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
